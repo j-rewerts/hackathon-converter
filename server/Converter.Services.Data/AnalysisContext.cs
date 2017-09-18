86fe3b3823b1789b49f3af3d7256b03679716d20
dchenier <daniel.chenier@edmonton.ca>
2017-09-18 13:25:09 -0600

    Skeleton server.

---
+++ server/Converter.Services.Data/AnalysisContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converter.Services.Data
+{
+    public class AnalysisContext : DbContext
+    {
+        public AnalysisContext() : base() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // TODO: setup fluent API stuff 
+        }
+    }
+}
---
+++ server/Converter.Services.WebApi/Controllers/AnalysisController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Converter.Services.WebApi.Controllers
+{
+    // http://localhost:24822/Analysis/Start/abc
+    [Route("[controller]")]
+    public class AnalysisController : Controller
+    {
+        [HttpPost("[action]/{:id}")]
+        public IActionResult Start(string id)
+        {
+            // TODO: Call the Converter.Services.OpenXml project to analyze the files
+            throw new NotImplementedException();
+
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Retrieve()
+        {
+            // TODO: implement
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]/{:id}")]
+        public IActionResult Retrieve(string id)
+        {
+            // TODO: implement
+            throw new NotImplementedException();
+        }
+    }
+}
