2b90671e481c2c71444c81c429f4ab0e5dd5228f
dchenier <daniel.chenier@edmonton.ca>
2017-09-19 09:28:07 -0600

    Checkpoint

---
+++ server/Converter.Services.Data/AnalysisRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Services.Data
+{
+    internal class AnalysisRepository : IAnalysisRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <remarks>context is typically provided through dependency injection</remarks>
+        public AnalysisRepository(AnalysisContext context)
+        {
+            this._context = context;
+        }
+
+        private readonly AnalysisContext _context;
+
+        public async Task<int> AddAnalysisAsync(string googleFileId)
+        {
+            var wb = new Workbook();
+            // TODO; set workbook properties here (maybe just fileId?)
+            _context.Workbooks.Add(wb);
+            await _context.SaveChangesAsync();
+
+            return wb.Id;
+        }
+    }
+}
---
+++ server/Converter.Services.Data/IAnalysisRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Services.Data
+{
+    public interface IAnalysisRepository
+    {
+        Task<int> AddAnalysisAsync(string googleFileId);
+
+        // TODO: add methods for retrieving analysis results
+
+        // We should probably make the Entity Framework classes internal
+        // and create new public classes for returning the values.
+        // (prevents leaky abstractions).
+        // We can use Automapper to map between the types.
+    }
+}
