9e193c5e013a49770bfa5675cba3d26c6e59fdb9
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-09-20 16:43:52 +0000

    Added a mock context for testing.

---
+++ server/Converter.Services.Tests/MockAnalysisContext.cs
@@ -0,0 +1,27 @@
+﻿using Converter.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Converter.Services.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Converter.Services.Tests
+{
+    class MockAnalysisContext : IAnalysisContext
+    {
+        public DbSet<Analysis> Analysis { get; set; }
+        public DbSet<Cell> Cell { get; set; }
+        public DbSet<CellIssue> CellIssue { get; set; }
+        public DbSet<IssueType> IssueType { get; set; }
+        public DbSet<Workbook> Workbook { get; set; }
+        public DbSet<WorkbookIssue> WorkbookIssue { get; set; }
+        public DbSet<Worksheet> Worksheet { get; set; }
+
+        public async Task SaveChangesAync()
+        {
+            // mock implementation does nothing
+        }
+
+    }
+}
