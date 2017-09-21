f73f420b14872a4f3258b05d17dffbd5f7af2d8d
Ben Nakaska <bnakaska@atb.com>
2017-09-21 10:06:12 -0600

    added not excel file test.

--- server/Converter.Services.Tests/ExcelReaderTests.cs
+++ server/Converter.Services.Tests/ExcelReaderTests.cs
@@ -26,8 +26,29 @@ namespace Converter.Services.Tests
         public void ReadExternalExcelSheet()
         {
             // open test excel file
-            string excelFile = @"C:\Users\bnaka\Desktop\Finance\3883FormSearchRegistrationDischarge.xlsm";
+            //string excelFile = @"C:\Users\bnaka\Desktop\Finance\3883FormSearchRegistrationDischarge.xlsm";
+            string excelFile = @"C:\Users\bnaka\Desktop\Finance\Linked to excel.xlsx";
+            using (var stream = new FileStream(path: excelFile, mode: FileMode.Open))
+            {
+                var reader = new ExcelReader(stream, "ASDASDASD_SADDSA");
+                var allItems = reader.ReadFile().ToList();
 
+                var EmptyOnes = allItems.Where(x => string.IsNullOrWhiteSpace(x.Value)).ToList();
+                var nonEmptyOnes = allItems.Where(x => !string.IsNullOrWhiteSpace(x.Value)).ToList();
+                var formulaOnes = allItems.Where(x => !string.IsNullOrWhiteSpace(x.Formula)).ToList();
+                var cellCount = reader.GetWorksheets().Sum(x => x.CellCount);
+                var sheetCount = reader.GetWorksheets().Count();
+                var formulaCount = allItems.Count(x => !string.IsNullOrWhiteSpace(x.Formula));
+                var test = "";
+            }
+        }
+        [TestMethod]
+        [ExpectedException(typeof(NotSpreadSheetException))]
+        public void NotValidExcelFile()
+        {
+            // open test excel file
+            //string excelFile = @"C:\Users\bnaka\Desktop\Finance\3883FormSearchRegistrationDischarge.xlsm";
+            string excelFile = @"C:\Users\bnaka\Desktop\Finance\Linked to excel.xls";
             using (var stream = new FileStream(path: excelFile, mode: FileMode.Open))
             {
                 var reader = new ExcelReader(stream, "ASDASDASD_SADDSA");
