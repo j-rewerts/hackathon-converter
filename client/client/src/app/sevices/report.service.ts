e30f00c743ed915839bf5a542e2d6fb3c4afa8de
qiz264 <qzhu@atb.com>
2017-09-21 14:04:15 -0600

    changed to use material expansion panel

--- client/client/src/app/sevices/report.service.ts
+++ client/client/src/app/sevices/report.service.ts
@@ -3,7 +3,6 @@ import { Response } from '@angular/http';
 import { HttpInterceptorService } from '@covalent/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { Subject } from 'rxjs/Subject';
 
 import { API_ENDPOINT } from '../config/app.config';
 import { IFile } from '../features/report/interfaces/report.interface';
@@ -11,20 +10,15 @@ import { IFile } from '../features/report/interfaces/report.interface';
 @Injectable()
 export class ReportService {
 
-  analyzeSuccess$: Subject<void>;
-
-  constructor(private httpService: HttpInterceptorService) {
-    this.analyzeSuccess$ = new Subject<void>();
-  }
+  constructor(private httpService: HttpInterceptorService) {}
 
   analyzeFile(data: {}): Observable<any> {
     return this.httpService.post(API_ENDPOINT.ANALYZE_FILE, data)
-      .map((res: Response) => this.analyzeSuccess$.next());
+      .map((res: Response) => res);
   }
 
   getReports(): Observable<IFile[]> {
     return this.httpService.get(API_ENDPOINT.GET_REPORTS)
       .map((res: Response) => res.json().tasks);
-      // .map((res: Response) => [res.json()]);
   }
 }
