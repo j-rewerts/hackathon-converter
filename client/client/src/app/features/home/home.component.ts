ee4705c29f0708203c8a2f1f921d3dafd1025e7a
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-09-22 09:29:43 -0600

    Added a home page.

--- client/client/src/app/features/home/home.component.ts
+++ client/client/src/app/features/home/home.component.ts
@@ -12,4 +12,11 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  redirectInstall() {
+    window.open('https://chrome.google.com/webstore/detail/hacksaw/nopnplelmjnfonhfhmhnpdebmbgkcdhb', '_blank');
+  }
+
+  redirectShare() {
+    window.open('https://twitter.com/intent/tweet?text=Got%20Office%3F%20Check%20out%20%40Hacksaw.%20%23ATBHackathon', '_blank');
+  }
 }
