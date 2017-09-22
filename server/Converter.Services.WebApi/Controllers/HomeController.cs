a4ea26af466b01b53e7cf8fdbc9f8c2c45ca99e4
Dan Chenier <daniel.chenier@edmonton.ca>
2017-09-22 07:31:39 -0600

    added default reoute to Angular app for routing

---
+++ server/Converter.Services.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Converter.Services.WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // based on https://stackoverflow.com/questions/42414397/asp-net-core-mvc-catch-all-route-serve-static-file
+        public IActionResult Spa()
+        {
+            return File("~/index.html", "text/html");
+        }
+    }
+}
